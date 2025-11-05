fe232a5761fb966294316cf1a83fbdc48a5c1568
Ana Martins <a.i.s.martins@astro.uio.no>
2025-11-05 13:01:08 +0100

    Refactor noise handling in simulations and add new noise generation methods; update related scripts and fix bugs in CG mapmaker calculations.

--- src/globals.py
+++ src/globals.py
@@ -10,3 +10,7 @@ NPIX = 12 * NSIDE**2
 SIM_TYPE = "modern"  # "firas" or "modern"
 NPIXPERIFG = 512
 N_IFGS = 16
+
+NOISE = "only white"  # "only white" or "1f"
+
+F_NYQ = 112  # Hz (LLSS from FIRAS)
