1ee57a0eb970a1133899ff3b3734e524ca972b8e
Ana Martins <a.i.s.martins@astro.uio.no>
2025-12-16 13:57:42 +0100

    updated to have full parameters

--- src/plotting/fossil_scan.py
+++ src/plotting/fossil_scan.py
@@ -12,19 +12,19 @@ obs_eff = 0.7
 days = int(survey_len * 365.25 * obs_eff)
 cum_hit_map = np.zeros(hp.nside2npix(g.NSIDE["fossil"]))
 
-for day in range(1, days):
-    hit_map = hp.read_map(f"{path}day_{day:03d}.fits")
-    hp.mollview(hit_map, title=f"FOSSIL Hit Map - Day {day:03d}", unit="Hits per pixel")
-    plt.savefig(f"{path}day_{day:03d}.png")
+for day in range(1, days+1):
+    hit_map = hp.read_map(f"{path}day_{day:04d}.fits")
+    hp.mollview(hit_map, title=f"FOSSIL Hit Map - Day {day:04d}", unit="Hits per pixel")
+    plt.savefig(f"{path}day_{day:04d}.png")
     plt.close()
 
     cum_hit_map += hit_map
-    hp.mollview(cum_hit_map / max(cum_hit_map), title=f"FOSSIL Cumulative Hit Map - Day {day:03d}",
+    hp.mollview(cum_hit_map / max(cum_hit_map), title=f"FOSSIL Cumulative Hit Map - Day {day:04d}",
                 unit="Normalized Hits", coord="E", min=0, max=1)
-    plt.savefig(f"{path}cumulative_day_{day:03d}_ecliptic.png")
+    plt.savefig(f"{path}cumulative_day_{day:04d}_ecliptic.png")
     plt.close()
 
-    hp.mollview(cum_hit_map / max(cum_hit_map), title=f"FOSSIL Cumulative Hit Map - Day {day:03d}",
+    hp.mollview(cum_hit_map / max(cum_hit_map), title=f"FOSSIL Cumulative Hit Map - Day {day:04d}",
                 unit="Normalized Hits", coord=["E", "G"], min=0, max=1)
-    plt.savefig(f"{path}cumulative_day_{day:03d}_galactic.png")
+    plt.savefig(f"{path}cumulative_day_{day:04d}_galactic.png")
     plt.close()
