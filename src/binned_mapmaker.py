Refactor binned_mapmaker.py for improved readability and performance; update fossil.py to correct dust and bb subtraction in IFG calculation

src/binned_mapmaker.py
@@ -1,16 +1,11 @@
-import os
-import sys
+from time import time as _time
 
 import healpy as hp
 import matplotlib.pyplot as plt
 import numpy as np
 
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
-sys.path.append(parent)
-from time import time as _time
-
 import globals as g
+import spectra
 import utils
 from argparser import args
 
@@ -38,37 +33,31 @@ else:
 t0 = utils.log_step("select_middle_pixel", t0, args.run_name)
 
 # plot hit map of the scanning strategy
-hit_map = np.bincount(pix, minlength=g.NPIX[g.SIM_TYPE]).astype(float)
+npix = g.NPIX[g.SIM_TYPE]
+hit_map = np.bincount(pix, minlength=npix).astype(float)
 mask = hit_map == 0
 hit_map[mask] = hp.UNSEEN
 t0 = utils.log_step("create_hit_map", t0, args.run_name)
 if g.PNG:
-    hp.mollview(
-        hit_map,
-        title="Scanning strategy hit map",
-        unit="Number of hits over the full mission",
-        min=0,
-        max=hit_map.max(),
-        xsize=2000,
-        coord=["E", "G"],
-    )
+    hp.mollview(hit_map, title="Scanning strategy hit map",
+                unit="Number of hits over the full mission", min=0, max=hit_map.max(), xsize=2000,
+                coord=["E", "G"])
     plt.savefig("../output/hit_maps/binned.png")
     plt.close()
 
     print("Saved hit map of the scanning strategy to ../output/hit_maps/binned.png.")
 t0 = utils.log_step("plot_hit_map", t0, args.run_name)
 
-npix = g.NPIX[g.SIM_TYPE]
+
 pix = pix.astype(np.int64, copy=False)
 
 m_ifg = np.zeros((npix, g.IFG_SIZE[g.SIM_TYPE]), dtype=float)
 
 # Vectorized accumulation is much faster than looping in Python.
 np.add.at(m_ifg, pix, ifgs)
-data_density = np.bincount(pix, minlength=npix).astype(float)
 
-mask = data_density == 0
-np.divide(m_ifg, data_density[:, np.newaxis], out=m_ifg, where=~mask[:, np.newaxis])
+mask = hit_map == 0
+np.divide(m_ifg, hit_map[:, np.newaxis], out=m_ifg, where=~mask[:, np.newaxis])
 m_ifg[mask] = np.nan
 t0 = utils.log_step("bin_ifgs", t0, args.run_name)
 
@@ -76,8 +65,8 @@ m = np.fft.rfft(m_ifg, axis=1)
 t0 = utils.log_step("rfft", t0, args.run_name)
 # m_abs = np.abs(m)
 
-frequencies = utils.generate_frequencies(nfreq=g.SPEC_SIZE[g.SIM_TYPE], simtype=g.SIM_TYPE)
-bb = utils.planck(frequencies, temp=2.7)
+frequencies = spectra.generate_frequencies(nfreq=g.SPEC_SIZE[g.SIM_TYPE], simtype=g.SIM_TYPE)
+bb = spectra.planck(frequencies, temp=2.7)
 m_abs = m + bb[np.newaxis, :]
 t0 = utils.log_step("bb_addition", t0, args.run_name)
 

src/sims/fossil.py
@@ -57,7 +57,7 @@ dust = dust_map_Mjy[:, np.newaxis] * sed[np.newaxis, :]
 bb = spectra.planck(frequencies, temp=2.7)
 t0 = utils.log_step("planck + dust multiplication", t0, args.run_name)
 
-ifg = fft.irfft(dust)# - bb, axis=1)
+ifg = fft.irfft(dust - bb, axis=1)
 t0 = utils.log_step("irfft", t0, args.run_name)
 ifg = np.roll(ifg, 180, axis=1)
 ifg = ifg.real